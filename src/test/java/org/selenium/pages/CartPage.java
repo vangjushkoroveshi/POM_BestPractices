0649e1f2d0056fd3f3c851b5e1316945ed2029d1
vangjush <vangjush.koroveshi@moveoalbania.com>
2021-09-10 10:39:37 +0200

    - page factory

--- src/test/java/org/selenium/pages/CartPage.java
+++ src/test/java/org/selenium/pages/CartPage.java
@@ -1,21 +1,24 @@
 package org.selenium.pages;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.selenium.base.BasePage;
 
 public class CartPage extends BasePage {
-    private By productName = By.cssSelector("td[class='product-name'] a");
-    private By checkoutBtn = By.cssSelector(".checkout-button");
-
+    @FindBy(css = "td[class='product-name'] a") private WebElement productName;
+    @FindBy(css = ".checkout-button") @CacheLookup private WebElement checkoutBtn;
 
     public CartPage(WebDriver driver) {
         super(driver);
+        PageFactory.initElements(driver, this);
     }
 
     public String getProductName(){
-        return wait.until(ExpectedConditions.visibilityOfElementLocated(productName)).getText();
+        return wait.until(ExpectedConditions.visibilityOf(productName)).getText();
     }
 
     public CheckoutPage checkout(){
