d77114eb0c91718470045ada14a3468b73efb202
vangjush <vangjush.koroveshi@moveoalbania.com>
2021-09-10 10:56:02 +0200

    - Implement DriverManager

--- src/test/java/org/selenium/factory/DriverManager.java
+++ src/test/java/org/selenium/factory/DriverManager.java
@@ -1,12 +1,13 @@
 package org.selenium.factory;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DriverManager {
 
     public WebDriver initializeDriver(){
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+"\\browser\\chromedriver.exe");
+        WebDriverManager.chromedriver().cachePath("Drivers").setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         return driver;
