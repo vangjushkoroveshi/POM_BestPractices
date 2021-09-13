bb3c93f7656609344cce6289740248f2e6598945
vangjush <vangjush.koroveshi@moveoalbania.com>
2021-09-13 14:26:18 +0200

    - API implementation for user registration and addToCard functionality

---
+++ src/test/java/org/selenium/api/actions/DummyClass.java
@@ -0,0 +1,23 @@
+package org.selenium.api.actions;
+
+import org.selenium.objects.User;
+import org.selenium.utils.FakerUtils;
+
+public class DummyClass {
+
+    public static void main(String[] args) {
+
+        String username = "demouser"+ new FakerUtils().generateRandomNumber();
+        User user = new User().
+                setUsername(username).
+                setPassword("demopwd").
+                setEmail(username+"@gmail.com");
+        SignUpApi signUpApi = new SignUpApi();
+        signUpApi.register(user);
+        System.out.println("REGISTER COOKIES: "+signUpApi.getCookies());
+
+        CartApi cartApi = new CartApi(signUpApi.getCookies());
+        cartApi.addToCard(1215, 1);
+        System.out.println("CART COOKIES"+cartApi.getCookies());
+    }
+}
