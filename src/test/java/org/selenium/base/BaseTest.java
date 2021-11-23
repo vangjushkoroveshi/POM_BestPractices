43c4c30d6d6c1385cdb656e14ca2651a93b582bb
vangjush <vangjush.koroveshi@moveoalbania.com>
2021-11-23 11:17:49 +0100

    - Take Screenshot of the page

--- src/test/java/org/selenium/base/BaseTest.java
+++ src/test/java/org/selenium/base/BaseTest.java
@@ -2,7 +2,10 @@ package org.selenium.base;
 
 
 import io.restassured.http.Cookies;
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Cookie;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.selenium.constants.DriverType;
 import org.selenium.factory.DriverManagerFactory;
@@ -10,11 +13,14 @@ import org.selenium.factory.DriverManagerOriginal;
 import org.selenium.factory.abstractFactory.DriverManagerAbstract;
 import org.selenium.factory.abstractFactory.DriverManagerFactoryAbstract;
 import org.selenium.utils.CookiesUtils;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 public class BaseTest {
@@ -49,7 +55,14 @@ public class BaseTest {
     }
 
     @AfterMethod
-    public synchronized void tearDown(){
+    @Parameters("browser")
+    public synchronized void tearDown(@Optional String browser, ITestResult result) throws IOException {
+        if (result.getStatus() == ITestResult.FAILURE){
+            File destFile = new File(System.getProperty("user.dir")+"\\screenshots"+ File.separator+ browser+ File.separator + result.getTestClass().getRealClass().getSimpleName()
+                    + "_" + result.getMethod().getMethodName()+ ".png");
+            takeScreenshot(destFile);
+        }
+
         getDriver().quit();
     }
 
@@ -58,5 +71,11 @@ public class BaseTest {
         for (Cookie cookie: seleniumCookies){
             getDriver().manage().addCookie(cookie);
         }
+    }
+
+    private void takeScreenshot(File destFile) throws IOException {
+        TakesScreenshot takesScreenshot = (TakesScreenshot) getDriver();
+        File srcFile = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(srcFile,destFile);
     }
 }
--- src/test/java/org/selenium/dataProviders/MyDataProvider.java
+++ src/test/java/org/selenium/dataProviders/MyDataProvider.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 public class MyDataProvider {
 
-    @DataProvider(name = "getFeaturedProducts", parallel = true)
+    @DataProvider(name = "getFeaturedProducts", parallel = false)
     public Object[] getFeaturedProducts() throws IOException {
         return JacksonUtils.deserializeJson("products.json", Product[].class);
     }
